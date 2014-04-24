c82355b7d1c73751935cba984cbfe4e989f2c006
jonathanjanssens <jonathan.janssens@gmail.com>
2014-04-24 16:40:31 +0100

    fixed bug where extractor url appeared twice

--- Menu.py
+++ Menu.py
@@ -28,21 +28,19 @@ class Menu:
                 self.internalLinks.append(a)
             else:
                 self.externalLinks.append(a)
-        if self.extractorUrl is not None:
-            for a in self.internalLinks:
-                a['href'] = self.extractorUrl + a['href']
-            for a in self.externalLinks:
-                a['href'] = self.extractorUrl + a['href']
 
     def make(self):
         internal = ''
         external = ''
+        extractorUrl = ''
+        if self. extractorUrl is not None:
+            extractorUrl = self.extractorUrl
         with open(self.templateFile, 'r') as html:
             for a in self.internalLinks:
-                temp = "<li><a href='%s'>%s</a></li>" % (a['href'], a.get_text())
+                temp = "<li><a href='%s%s'>%s</a></li>" % (extractorUrl, a['href'], a.get_text())
                 internal += temp
             for a in self.externalLinks:
-                temp = "<li><a href='%s'>%s</a></li>" % (a['href'], a.get_text())
+                temp = "<li><a href='%s%s'>%s</a></li>" % (extractorUrl, a['href'], a.get_text())
                 external += temp
             self.menuHTML = html.read().replace('{{ internal_origin_links }}', internal).replace('{{ external_origin_links }}', external)
         if self.extractorUrl is not None:
